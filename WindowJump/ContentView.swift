3a03f8dad67d0ec60bf420054439439dc4914b99
Marcus H <mhultjavad24@proton.me>
2026-06-29 22:40:16 +0200

    Initial Commit

---
+++ WindowJump/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WindowJump
+//
+//  Created by wake on 2026-06-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WindowJump/WindowJumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowJumpApp.swift
+//  WindowJump
+//
+//  Created by wake on 2026-06-29.
+//
+
+import SwiftUI
+
+@main
+struct WindowJumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
